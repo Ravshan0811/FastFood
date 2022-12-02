34a16a535bc6dac6214393b1c33844eacc1b07cd
Ravshan <oravshan26@gmail.com>
2022-12-02 23:49:14 +0500

    tugadi

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// let total = document.querySelector("#total");
+let som = 0;
 let text = document.querySelector("#name");
 let phone = document.querySelector("#phone");
 let address = document.querySelector("#address");
@@ -7,7 +7,7 @@ let size = document.querySelector("#size");
 let on = document.querySelector("#on");
 let add = document.querySelector("#add");
 
-function myfun (event) {
+function myfun () {
   let user = document.querySelector("#user");
   let tel = document.querySelector("#tel");
   let email = document.querySelector("#email");
@@ -16,33 +16,54 @@ function myfun (event) {
   let pizzaUz = document.querySelector("#pizzaUz");
   let pizza = document.querySelector("#pizza");
   
-  
   text.textContent += user.value
   phone.textContent += tel.value
   address.textContent += email.value
   dough.textContent += select.value
   if (fun0.children[0].children[0].checked) {
-    size.textContent += "25 sm"
+    size.textContent += "  25 sm"
+    som += 10;
   }
   if (fun0.children[1].children[0].checked) {
-    size.textContent += "30 sm"
+    size.textContent += "  30 sm"
+    som += 12
   }
   if (fun0.children[2].children[0].checked) {
-    size.textContent += "35 sm"
+    size.textContent += "  35 sm"
+    som += 15
   }
   if (pizzaUz.children[0].children[0].checked) {
-    on.textContent += "Turkey meat"
+    on.textContent += "  Turkey meat";
+    som += 5;
   }
   if (pizzaUz.children[1].children[0].checked) {
-    on.textContent += "Mushroom"
+    on.textContent += "  Mushroom"
+    som += 5
   }
   if (pizzaUz.children[2].children[0].checked) {
-    on.textContent += "Olive"
+    on.textContent += "  Olive";
+    som += 5;
+  }
+  if (pizzaUz.children[3].children[0].checked) {
+    on.textContent += "  Horse meat";
+    som += 5;
   }
   if (pizza.children[0].children[0].checked) {
-    add.textContent += "Pepper"
+    add.textContent += "  Pepper";
+    som += 5;
   }
   if (pizza.children[1].children[0].checked) {
-    add.textContent += "Sausage cucumber"
+    add.textContent += "  Sausage cucumber";
+    som += 5 ;
   }
 }
+
+let total = document.querySelector("#total");
+let btn = document.querySelector("#btn");
+let wrapper = document.querySelector(".pizza__wrapper");
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  wrapper.classList.add("add");
+  total.textContent += som;
+});
